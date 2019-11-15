d73e59778d1b78aa2bffcfa4caba2445e373542d
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-11-15 23:53:23 +0900

    Add char_stream to lexer.cpp

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <string>
 #include <vector>
 #include <exception>
@@ -42,3 +43,35 @@ int Token::get_as_integer() const {
 TokenStream::TokenStream(const std::vector<Token>& tokens):
   stream(tokens)
 {}
+
+
+
+struct char_stream {
+  explicit char_stream() {}
+  bool is_valid() {
+    return index <= cur_line.length();
+  }
+  bool open(std::string const &filename) {
+    ifs.open(filename);
+    if (!ifs) {
+      return false;
+    }
+    std::getline(ifs, cur_line);
+    index = 0;
+    return true;
+  }
+  char get_next_token() {
+    if (index < cur_line.length()) {
+      return cur_line[index++];
+    }
+    if (!ifs) {
+      return '\0';
+    }
+    std::getline(ifs, cur_line);
+    index = 0;
+    return cur_line[0];
+  }
+  std::ifstream ifs;
+  std::string cur_line;
+  size_t index;
+};
