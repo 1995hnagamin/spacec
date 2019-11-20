28598789c261ae8de133f9c1e3e1834baacbef0d
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-11-21 00:18:38 +0900

    Introduce show_tokens() to spccc.cpp

--- src/spccc.cpp
+++ src/spccc.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <vector>
+#include "lexer.hpp"
 
 void show_help() {
   std::cout << "usage: spccc <filename>" << std::endl;
+}
+
+void show_tokens(std::vector<Token> const &tokens) {
+  for (auto &&token : tokens) {
+    std::cout << token.representation() << std::endl;
+  }
 }
 
 int main(int argc, char **argv) {
@@ -9,5 +17,8 @@ int main(int argc, char **argv) {
     show_help();
     return 0;
   }
+
+  auto const tokens = LexicalAnalysis(argv[1]);
+  show_tokens(tokens);
   return 0;
 }
