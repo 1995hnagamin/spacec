850f61a3cd34ae6315b57c3eef6228ad894b293a
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-11-21 00:17:43 +0900

    Introduce show_help()

--- src/spccc.cpp
+++ src/spccc.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
-int main(int, char **) {
+void show_help() {
+  std::cout << "usage: spccc <filename>" << std::endl;
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    show_help();
+    return 0;
+  }
   return 0;
 }
